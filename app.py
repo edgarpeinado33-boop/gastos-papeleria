7a6a3c02ba447e6bcd1eab81e0c54dce993ac512
edgarpeinado33-boop <edgarpeinado33@gmail.com>
2026-05-28 13:08:42 -0400

    noveno_quinto_commit

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
-from flask import Flask, send_from_directory
+from flask import Flask
 from controllers.gasto_controller import GastoController
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-app = Flask(__name__, template_folder='views', static_folder='static')
+app = Flask(__name__, template_folder='views', static_folder='views/static')
 
-# Servir archivos estáticos manualmente
-@app.route('/static/<path:filename>')
-def custom_static(filename):
-    return send_from_directory('views/static', filename)
+# Asegurar que los archivos estáticos se sirvan correctamente
+app.static_folder = 'views/static'
+app.static_url_path = '/static'
 
 @app.route('/')
 def index():
@@ -23,6 +22,11 @@ def nuevo_gasto():
 @app.route('/reportes')
 def reportes():
     return GastoController.reportes()
+
+# Para Vercel - Forzar que sirva archivos estáticos
+@app.route('/static/<path:filename>')
+def serve_static(filename):
+    return app.send_static_file(filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
